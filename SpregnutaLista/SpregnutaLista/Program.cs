0dc36c12b1c687209bd9d4252ba559ecf0c29a2d
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2017-10-19 17:09:33 +0200

    Suvoparna prva uploadacija novoga zadatka

---
+++ SpregnutaLista/SpregnutaLista/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpregnutaLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista prvi = new Lista();
+            prvi.AddToEnd(new Ucenik("Pero jebeni", "Peric"));
+            prvi.AddToEnd(new Ucenik("Pero jebeni", "Peric 2"));
+            prvi.AddToStart(new Ucenik("Pero jebeni", "Peric najveci"));
+            prvi.AddToEnd(new Ucenik("Pero jebeni", "Peric 3"));
+            prvi.Obrisi(2);
+            prvi.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
