6ad86b78836ac87946b55d3a147492ddce46ace2
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2017-11-30 14:54:37 +0100

    AddedStacks

---
+++ Liste/TestConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Liste;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedCircularList<int> Lista = new LinkedCircularList<int>();
+            Lista.Add(3);
+            Lista.Add(4);
+            Lista.Add(5);
+            Lista.Add(6);
+            Lista.Print();
+            Lista.ReplaceFirstAndLast();
+            Lista.Print();
+            Lista.DeleteFirstAndLast();
+            Lista.Print();
+            Console.ReadKey();
+        }
+    }
+}
